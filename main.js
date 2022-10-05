e6d0da182a98bc762ddee3d33e54284221f26b76
b.anwar <109045924+fatist13@users.noreply.github.com>
2022-10-05 13:48:09 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
 // JavaScript Code
 // Input
-let userName = prompt(“Please enter your name”);
+let userName = prompt(Please ,enter, your, name);
 
-// Check userName
-if (userName === “Mr. V”) {
-	alert(“Wow, we have the same name.”);
+if (userName === Mr. V) {
+	alert(Wow, we, have, the, same, name.);
 } else {
-	alert(“We don’t have the same name.”);
+	alert(We, don’t, have, the, same, name.);
 }
