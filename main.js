f92d62ff4732e1beb8bc04bf6a1119b809263d70
b.anwar <109045924+fatist13@users.noreply.github.com>
2022-10-05 14:40:55 -0600

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ let providedGrade = prompt("Please enter a Grade");
 // Check userName
 if (providedGrade >= 90) {
   alert("You will do well in the next level");
-} else if ((providedGrade = 70 - 89)) {
+} else if ((providedGrade >= 70) & 89) {
   alert("You should do well in the next level");
 } else if ((providedGrade = 50 - 69)) {
   alert("The next level may be difficult for you");
